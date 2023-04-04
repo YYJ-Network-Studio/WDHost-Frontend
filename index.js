f0a39b4f1b89c0f8aea7dc20ea751ae6e142fd1e
YYJ <68520446+YYJ-TW@users.noreply.github.com>
2023-04-04 22:55:03 +0800

    Redesign service block and footer using Bootstrap

--- index.js
+++
@@ -1,8 +0,0 @@
-const themeToggle = document.querySelector('#theme');
-const body = document.querySelector('body');
-const footer = document.querySelector('footer');
-
-themeToggle.addEventListener('click', () => {
-  body.classList.toggle('light-mode');
-  footer.classList.toggle('light-mode');
-});
